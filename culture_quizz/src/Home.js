10e52a7846f90f1f013a74c22917c3db3126c230
JOUTIN <jules.outin@ynov.com>
2023-04-08 10:10:11 +0200

    Mise en place des catégories

--- culture_quizz/src/Home.js
+++ culture_quizz/src/Home.js
@@ -9,6 +9,18 @@ function App() {
       <p className="Textpresentation">
         Bienvenue sur Culture Quizz, le site qui vous permet de tester vos connaissances sur la culture générale.
       </p>
+      <div className="Theme">
+        <div className="Option">
+          <button type="submit">Option 1</button>
+        </div>
+        <div className="Option"> 2</div>
+        <div className="Option"> 3</div>
+        <div className="Option"> 4</div>
+        <div className="Option"> 5</div>
+        <div className="Option"> 6</div>
+        <div className="Option"> 7</div>
+        <div className="Option"> 8</div>
+      </div>
     </div>
   );
 }
