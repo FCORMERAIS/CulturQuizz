bbffbb3259e2730fbb231ab42f0a5e9ea33de2db
JOUTIN <jules.outin@ynov.com>
2023-04-18 16:16:54 +0200

    Page principale

--- culture_quizz/src/Home.js
+++ culture_quizz/src/Home.js
@@ -1,6 +1,35 @@
 import './App.css';
 
 function App() {
+
+  // fonction pour ajouter automatiquement des category
+  // const addCategory = () => {
+  //   let i = 2;
+  //   let category = document.createElement('div');
+  //   category.className = 'Category';
+  //   let option = document.createElement('div');
+  //   option.className = 'Option';
+  //   let themes = {
+  //     0: 'Histoire',
+  //     1: 'Géographie',
+  //     2: 'Sport',
+  //     3: 'Musique',
+  //     4: 'Cinéma',
+  //     5: 'Littérature',
+  //     6: 'Sciences',
+  //     7: 'Art',
+  //     8: 'Histoire',
+  //   }
+  //   let theme = document.createElement('a');
+  //   theme.className = 'Theme';
+  //   theme.href = './theme' + i;
+  //   theme.innerHTML = themes[i];
+  //   option.appendChild(theme);
+  //   category.appendChild(option);
+  //   document.body.appendChild(category);
+  // }
+  // addCategory()
+  
   return (
     <div>
       <h1 className="Title">
@@ -9,21 +38,21 @@ function App() {
       <p className="Textpresentation">
         Bienvenue sur Culture Quizz, le site qui vous permet de tester vos connaissances sur la culture générale.
       </p>
-      <h1>
+      <h1 className='TitleCategory'>
         Liste des catégories
+          <div className='Category'>
+            <div className='Option'>
+              <a className='Theme' href='./theme0'>
+                Histoire
+              </a>
+            </div>
+            <div className='Option'>
+              <a className='Theme' href='./theme0'>
+                Sport
+              </a>
+            </div>
+          </div>
       </h1>
-      <div className="Theme">
-        <div className="Option">
-          <button type="submit">Option 1</button>
-        </div>
-        <div className="Option"> 2</div>
-        <div className="Option"> 3</div>
-        <div className="Option"> 4</div>
-        <div className="Option"> 5</div>
-        <div className="Option"> 6</div>
-        <div className="Option"> 7</div>
-        <div className="Option"> 8</div>
-      </div>
     </div>
   );
 }
